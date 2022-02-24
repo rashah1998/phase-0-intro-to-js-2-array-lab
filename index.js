68c99de18546165562fd5d0abb48aa2f588e977a
Rahul Shah <rashah1998@yahoo.com>
2022-02-24 17:31:30 -0500

    commit tree 5ff61c9a5f9bdc67d24063ec1faab6836d41f417

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
 // Write your solution here!
+const cats = ['Milo', 'Otis', 'Garfield'];
+
+function destructivelyAppendCat(name) {
+    return cats.push(name);
+}
+
+function destructivelyPrependCat(name) {
+    return cats.unshift(name);
+}
+
+function destructivelyRemoveLastCat() {
+    return cats.pop();
+}
+
+function destructivelyRemoveFirstCat() {
+    return cats.shift();
+}
+
+function appendCat(name) {
+    const appendedCats = [...cats, name];
+    return appendedCats;
+}
+
+function prependCat(name) {
+    const prependedCats = [name, ...cats];
+    return prependedCats;
+}
+
+function removeLastCat() {
+    const lastCatRemoved = [...cats.slice(0, -1)];
+    return lastCatRemoved;
+}
+
+function removeFirstCat() {
+    const firstCatRemoved = [...cats.slice(1)];
+    return firstCatRemoved;
+}
